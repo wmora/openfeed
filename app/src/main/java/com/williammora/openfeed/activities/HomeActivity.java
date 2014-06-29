85c3614bfb491b015f3df01869cbc9349e5df645
William Mora <wilmor24@gmail.com>
2014-06-28 23:21:52 -0300

    Refactor

--- app/src/main/java/com/williammora/openfeed/activities/HomeActivity.java
+++ app/src/main/java/com/williammora/openfeed/activities/HomeActivity.java
@@ -11,12 +11,12 @@ import android.view.ViewGroup;
 
 import com.williammora.openfeed.R;
 
-public class WelcomeActivity extends Activity {
+public class HomeActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_welcome);
+        setContentView(R.layout.activity_home);
         if (savedInstanceState == null) {
             getFragmentManager().beginTransaction()
                     .add(R.id.container, new PlaceholderFragment())
@@ -28,7 +28,7 @@ public class WelcomeActivity extends Activity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.welcome, menu);
+        getMenuInflater().inflate(R.menu.home, menu);
         return true;
     }
 
@@ -55,7 +55,7 @@ public class WelcomeActivity extends Activity {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_welcome, container, false);
+            View rootView = inflater.inflate(R.layout.fragment_home, container, false);
             return rootView;
         }
     }
