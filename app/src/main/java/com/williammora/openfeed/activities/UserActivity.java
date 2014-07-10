163feaf2d03d60cd92f8ef3b7dd0200c0f62a888
William Mora <wilmor24@gmail.com>
2014-07-10 20:44:29 -0300

    User activity finishes on back pressed

--- app/src/main/java/com/williammora/openfeed/activities/UserActivity.java
+++ app/src/main/java/com/williammora/openfeed/activities/UserActivity.java
@@ -2,7 +2,6 @@ package com.williammora.openfeed.activities;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.MenuItem;
 
 import com.williammora.openfeed.R;
@@ -21,4 +20,16 @@ public class UserActivity extends Activity {
         }
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
