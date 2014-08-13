335b0069c1efff8853d8a029d449cf9960cf65bd
William Mora <wilmor24@gmail.com>
2014-08-12 23:43:03 -0300

    Refactor

--- app/src/main/java/com/williammora/openfeed/activities/WelcomeActivity.java
+++ app/src/main/java/com/williammora/openfeed/activities/WelcomeActivity.java
@@ -1,6 +1,5 @@
 package com.williammora.openfeed.activities;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -10,9 +9,8 @@ import com.williammora.openfeed.R;
 import com.williammora.openfeed.events.TwitterEvents;
 import com.williammora.openfeed.fragments.WelcomeFragment;
 import com.williammora.openfeed.services.TwitterService;
-import com.williammora.openfeed.utils.BusProvider;
 
-public class WelcomeActivity extends Activity {
+public class WelcomeActivity extends OpenFeedActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,19 +33,11 @@ public class WelcomeActivity extends Activity {
     @Override
     protected void onResume() {
         super.onResume();
-        BusProvider.getInstance().register(this);
-
         // Check if we are coming back form authorizing Twitter
         Uri uri = getIntent().getData();
         if (uri != null && uri.toString().startsWith(getString(R.string.twitter_callback_url))) {
             handleTwitterCallback(uri);
         }
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        BusProvider.getInstance().unregister(this);
     }
 
     private void handleTwitterCallback(Uri uri) {
